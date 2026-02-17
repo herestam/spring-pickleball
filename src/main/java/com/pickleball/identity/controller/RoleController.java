c9b048ba267ee2bba7ee0c2bd14eb0013dd64c23
tamnn25 <80872048+tamnn25@users.noreply.github.com>
2026-02-17 08:09:35 +0700

    Remove comment and add collection

--- src/main/java/com/pickleball/identity/controller/RoleController.java
+++ src/main/java/com/pickleball/identity/controller/RoleController.java
@@ -1,10 +1,15 @@
 package com.pickleball.identity.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pickleball.identity.dto.RoleRequest;
 import com.pickleball.identity.dto.RoleResponse;
+import com.pickleball.identity.model.Role;
 import com.pickleball.identity.service.RoleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +24,36 @@ public class RoleController {
 
     private final RoleService roleService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
     @GetMapping
     public ResponseEntity<List<RoleResponse>> getAllRoles() {
+        try{
+            List<Role> roles = roleService.getAllRoles();
+
+            List<Role> roll = roles.stream().filter(r -> "USER".equals(r.getName())).toList();
+
+            for (Role r : roll){
+                System.out.println("role user" + r.getName());
+            }
+
+            roles.forEach(r -> {
+                        System.out.println("ROles ===>" + r.getName());
+                        r.getPermissions().forEach(
+                                rp -> System.out.println(
+                                        "permission ==> debug" + rp.getPermission().getName()
+                                )
+                        );
+
+                    }
+            );
+        } catch (Exception e) {
+            System.out.println("failllllllll");
+            throw new RuntimeException(e);
+        }
+
         return ResponseEntity.ok(
                 roleService.getAllRoles().stream()
                         .map(RoleResponse::fromEntity)
